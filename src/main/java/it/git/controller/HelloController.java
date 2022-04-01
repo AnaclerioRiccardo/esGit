c39b4273fb074f3971dbaf49df82edd8808831c1
Anaclerio Riccardo <anaclerioriccardo@gmail.com>
2022-04-01 15:49:44 +0200

    inizio

---
+++ src/main/java/it/git/controller/HelloController.java
@@ -0,0 +1,14 @@
+package it.git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String saluta() {
+		return "ciao";
+	}
+
+}
